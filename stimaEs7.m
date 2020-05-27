f656aa63de8662387032e7a83c1fa97db4edbf45
Andrea Panno <31217389+badcortex@users.noreply.github.com>
2020-05-27 18:05:11 +0200

    Esercitazione7 added

---
+++ stimaEs7.m
@@ -0,0 +1,23 @@
+% Eta': 32
+% Stato salute: Normale
+% Sesso: F
+% Altezza: 1.64
+% Peso: 59.6
+
+% Parametri stimati sul soggetto
+
+beta_vd = [1.0912,1.8623,-0.010067];
+
+beta_t_emi_s = [5.897,-0.027995,-0.0087605];
+
+beta_t_emi_l = [39.6479,0.18046,-1.8282,-0.27093,0.049459,1.6539,-11.2105,0.017483,0.22397];
+
+beta_fraction = [0.74006,0.0011266];
+
+vol = 3.8357;
+temp_emi_short = 4.9963;
+temp_emi_long = 34.5287;
+fraction = 0.76502;
+
+
+
